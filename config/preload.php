316a42ccb857f9c4ed09e7b75a2bb444bc4adc66
bbldn05 <bobol.boris@gmail.com>
2021-09-17 13:45:14 +0300

    17.09.2021 13:45

--- config/preload.php
+++ config/preload.php
@@ -1,5 +1,7 @@
 <?php
 
-if (file_exists(dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php')) {
-    require dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php';
+$path = dirname(__DIR__) . '/var/cache/prod/App_KernelProdContainer.preload.php';
+
+if (true === file_exists($path)) {
+    require $path;
 }
